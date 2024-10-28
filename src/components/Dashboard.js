f26a8119e21a4b3f797c32f07d15fd42d4f7d916
chldntlr <a76636922@gmail.com>
2024-10-29 08:27:29 +0900

    웹캠 추가

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,37 +1,39 @@
+// Dashboard.js
 import React from 'react';
 import SystemLogChart from './SystemLogChart';
 import Calendar from './Calendar';
 import DataTable from './DataTable';
+import WebcamComponent from './Webcam'; // 웹캠 컴포넌트 추가
 import '../styles/Dashboard.css';
-import { FaUserCheck, FaUserTimes, FaClock, FaCheckCircle } from 'react-icons/fa'; // 아이콘 추가
+import { FaUserCheck, FaUserTimes, FaClock, FaCheckCircle } from 'react-icons/fa';
 
 function Dashboard() {
   return (
     <div className="dashboard">
       <div className="dashboard-cards">
         <div className="card">
-          <FaUserCheck className="icon" /> {/* 출석 아이콘 */}
+          <FaUserCheck className="icon" />
           <div className="card-content">
             <div className="card-title">출석</div>
             <div className="card-value">15명</div>
           </div>
         </div>
         <div className="card">
-          <FaUserTimes className="icon" /> {/* 결석 아이콘 */}
+          <FaUserTimes className="icon" />
           <div className="card-content">
             <div className="card-title">결석</div>
             <div className="card-value">5명</div>
           </div>
         </div>
         <div className="card">
-          <FaClock className="icon" /> {/* 지각 아이콘 */}
+          <FaClock className="icon" />
           <div className="card-content">
             <div className="card-title">지각</div>
             <div className="card-value">1명</div>
           </div>
         </div>
         <div className="card">
-          <FaCheckCircle className="icon" /> {/* 총 출석률 아이콘 */}
+          <FaCheckCircle className="icon" />
           <div className="card-content">
             <div className="card-title">총 출석률</div>
             <div className="card-value">75%</div>
@@ -42,6 +44,7 @@ function Dashboard() {
         <SystemLogChart />
         <Calendar />
       </div>
+      <WebcamComponent /> {/* 웹캠 컴포넌트 추가 */}
       <DataTable />
     </div>
   );
